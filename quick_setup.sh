836b231a84d7a0eb7b92541bc8e86e8d09a64469
Nantas Nardelli <nantas.nardelli@gmail.com>
2017-01-03 21:43:07 +0000

    Use v1.0-2 in quick_setup

--- quick_setup.sh
+++ quick_setup.sh
@@ -21,9 +21,9 @@ cp $TC_DIR/BWEnv/bin/*.dll ./
 cp $TC_DIR/BWEnv/bin/*.sh ./
 cp $TC_DIR/config/*.ini $SC_DIR/bwapi-data/
 cp $TC_DIR/maps/micro/ $SC_DIR/Maps/BroodWar/ -r
-curl -LO https://github.com/TorchCraft/TorchCraft/releases/download/v1.0-0/torchcraft-v1.0-0.zip
-unzip torchcraft-v1.0-0.zip
-cp torchcraft-v1.0-0/* ./
+curl -LO https://github.com/TorchCraft/TorchCraft/releases/download/v1.0-2/torchcraft-v1.0-2.zip
+unzip torchcraft-v1.0-2.zip
+cp torchcraft-v1.0-2/* ./
 
 if [[ `uname` == 'Darwin' ]]; then
     sed -i '' "s|^ai *=.*|ai=$SC_DIR/BWEnv.dll|" bwapi-data/bwapi.ini

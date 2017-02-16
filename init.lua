02401e65f4356c7d0cbea7168ecbcca53a123722
Vasil Khalidov <vkhalidov@fb.com>
2017-02-16 11:27:46 +0100

    added timeout parameters to the lua connect method

--- init.lua
+++ init.lua
@@ -68,23 +68,17 @@ function torchcraft:init(hostname, port)
     print('host: ' .. self.hostname .. ':' .. self.port)
 end
 
-function torchcraft:connect(port)
+function torchcraft:connect(port, send_timeout_ms, receive_timeout_ms)
     -- connect() should be called at the beginning of every game
     if self.hostname == nil or self.hostname == '' then
         self:init(nil, port)
     end
-    -- initialize socket connection
-    while not self.client:connected() do
-        local status, ret
-        status, ret = pcall(function()
-            return self.client:connect(self.hostname, self.port)
-        end)
-        if not status then
-            print('Socket error (' .. self.hostname .. ':' .. port ..
-                  '), retrying connection in 1 second: ', ret)
-            os.execute('sleep 1')
-        end
-    end
+    -- timeouts for send and receive operations
+    send_timeout_ms = send_timeout_ms or -1
+    receive_timeout_ms = receive_timeout_ms or -1
+    -- initialize socket connection, use the specified timeouts
+    self.client.connect(self.hostname, self.port, send_timeout_ms,
+        receive_timeout_ms)
 
     self.state = self.client.state
 

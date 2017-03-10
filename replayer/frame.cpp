f8ac06ae11008ef530616ff1a703dca40918d1ab
ebetica <ebetica0@gmail.com>
2017-03-10 07:20:39 -0800

    Fix frame diff bug where it crashes when all neutrals die

--- replayer/frame.cpp
+++ replayer/frame.cpp
@@ -213,7 +213,7 @@ std::istream& replayer::operator>>(std::istream& in, replayer::Frame& o) {
 
 namespace detail = replayer::detail;
 // This macro maps the member variables of Unit to some IDs
-#define _DOALL(F)       \
+#define _DOALL(F)      \
   F(x, 0)              \
   F(y, 1)              \
   F(health, 2)         \
@@ -244,10 +244,10 @@ namespace detail = replayer::detail;
   F(resources, 27)
 
 #define _DOALL_ON_ORDER(F) \
-  F(first_frame, 0)       \
-  F(type, 1)              \
-  F(targetId, 2)          \
-  F(targetX, 3)           \
+  F(first_frame, 0)        \
+  F(type, 1)               \
+  F(targetId, 2)           \
+  F(targetX, 3)            \
   F(targetY, 4)
 
 replayer::FrameDiff replayer::frame_diff(
@@ -266,8 +266,6 @@ replayer::FrameDiff replayer::frame_diff(
   df.actions = lhs->actions;
   df.resources = lhs->resources;
   for (auto it : lhs->units) { // Iterates across number of players
-    // We assume the number of players are the same.
-    // I think people never run out of units (or they lose)
     df.pids.push_back(it.first);
 
     // Set up the units list
@@ -275,7 +273,9 @@ replayer::FrameDiff replayer::frame_diff(
     std::vector<detail::UnitDiff>& ul = df.units.back();
 
     auto lhsu = it.second;
-    auto rhsu = rhs->units.at(it.first);
+    auto rhsu = rhs->units.find(it.first) == rhs->units.end()
+        ? std::vector<replayer::Unit>()
+        : rhs->units.at(it.first);
     std::sort(lhsu.begin(), lhsu.end(), detail::orderUnitByiD);
     std::sort(rhsu.begin(), rhsu.end(), detail::orderUnitByiD);
     auto rit = rhsu.begin();
@@ -292,7 +292,7 @@ replayer::FrameDiff replayer::frame_diff(
           lit.id == rit->id) { // Unit exists in both frames
         int32_t buffer = 0;
 // Fill out diffs for the int32_t variables
-#define _GEN_VAR(NAME, NUM)          \
+#define _GEN_VAR(NAME, NUM)         \
   buffer = lit.NAME - rit->NAME;    \
   if (buffer != 0) {                \
     du.var_ids.push_back(NUM);      \
@@ -302,26 +302,28 @@ replayer::FrameDiff replayer::frame_diff(
 #undef _GEN_VAR
         // Fill out diffs for orders
         for (size_t i = 0; i < lit.orders.size(); i++) {
-#define _GEN_VAR(NAME, NUM)                                   \
-  if (i >= rit->orders.size()) buffer = lit.orders[i].NAME;   \
-  else buffer = lit.orders[i].NAME - rit->orders[i].NAME;     \
-  if (buffer != 0) {                                          \
-    du.order_ids.push_back(5 * i + NUM);                      \
-    du.order_diffs.push_back(buffer);                         \
+#define _GEN_VAR(NAME, NUM)                            \
+  if (i >= rit->orders.size())                         \
+    buffer = lit.orders[i].NAME;                       \
+  else                                                 \
+    buffer = lit.orders[i].NAME - rit->orders[i].NAME; \
+  if (buffer != 0) {                                   \
+    du.order_ids.push_back(5 * i + NUM);               \
+    du.order_diffs.push_back(buffer);                  \
   }
           _DOALL_ON_ORDER(_GEN_VAR)
 #undef _GEN_VAR
         }
       } else { // Unit only exist in latter frame;
 // Fill out diffs for the int32_t variables
-#define _GEN_VAR(NAME, NUM)   \
+#define _GEN_VAR(NAME, NUM)  \
   du.var_ids.push_back(NUM); \
   du.var_diffs.push_back(lit.NAME);
         _DOALL(_GEN_VAR)
 #undef _GEN_VAR
         // Fill out diffs for orders
         for (size_t i = 0; i < lit.orders.size(); i++) {
-#define _GEN_VAR(NAME, NUM)             \
+#define _GEN_VAR(NAME, NUM)            \
   du.order_ids.push_back(5 * i + NUM); \
   du.order_diffs.push_back(lit.orders[i].NAME);
           _DOALL_ON_ORDER(_GEN_VAR)
@@ -345,7 +347,9 @@ replayer::Frame* detail::add(replayer::Frame* frame, replayer::FrameDiff* df) {
     auto pid = df->pids[i];
     f->units[pid] = std::vector<replayer::Unit>();
 
-    auto f_units = frame->units.at(pid);
+    auto f_units = frame->units.find(pid) == frame->units.end()
+        ? std::vector<replayer::Unit>()
+        : frame->units.at(pid);
     std::sort(f_units.begin(), f_units.end(), detail::orderUnitByiD);
 
     // Should be in order
@@ -366,7 +370,7 @@ replayer::Frame* detail::add(replayer::Frame* frame, replayer::FrameDiff* df) {
 
       for (size_t k = 0; k < du.var_diffs.size(); k++) {
         switch (du.var_ids[k]) { // assumes int32_t are 0 initted
-#define _SWITCHES(NAME, NUM)    \
+#define _SWITCHES(NAME, NUM)   \
   case NUM:                    \
     u.NAME += du.var_diffs[k]; \
     break;
@@ -380,7 +384,7 @@ replayer::Frame* detail::add(replayer::Frame* frame, replayer::FrameDiff* df) {
         auto order_n = du.order_ids[k] / 5;
         auto field_n = du.order_ids[k] % 5;
         switch (field_n) {
-#define _SWITCHES(NAME, NUM)                      \
+#define _SWITCHES(NAME, NUM)                     \
   case NUM:                                      \
     u.orders[order_n].NAME += du.order_diffs[k]; \
     break;
@@ -492,24 +496,25 @@ std::istream& replayer::operator>>(std::istream& in, detail::UnitDiff& o) {
 
 #define STRINGIFY2(X) #X
 #define STRINGIFY(X) STRINGIFY2(X)
-#define _TESTMSG(COND, MSG) if (!(COND)) { \
-  std::cerr << MSG << std::endl; \
-  return false; \
-} else
+#define _TESTMSG(COND, MSG)        \
+  if (!(COND)) {                   \
+    std::cerr << MSG << std::endl; \
+    return false;                  \
+  } else
 #define _TEST(COND) _TESTMSG(COND, "(" STRINGIFY(COND) ") not satisfied")
-#define _EQV(VAR, CODE) (f1 ## VAR) CODE == (f2 ## VAR) CODE
-#define _EQ(CODE) (f1) CODE == (f2) CODE
+#define _EQV(VAR, CODE) (f1##VAR) CODE == (f2##VAR)CODE
+#define _EQ(CODE) (f1) CODE == (f2)CODE
 bool detail::frameEq(replayer::Frame* f1, replayer::Frame* f2) {
   _TEST(_EQ(->reward));
   _TEST(_EQ(->is_terminal));
   _TEST(_EQ(->bullets.size()));
-  for (size_t i=0; i<f1->bullets.size(); i++) {
+  for (size_t i = 0; i < f1->bullets.size(); i++) {
     _TEST(_EQ(->bullets[i].type));
     _TEST(_EQ(->bullets[i].x));
     _TEST(_EQ(->bullets[i].y));
   }
   _TEST(f1->resources.size() == f2->resources.size());
-  for (size_t i=0; i<f1->resources.size(); i++) {
+  for (size_t i = 0; i < f1->resources.size(); i++) {
     _TEST(_EQ(->resources[i].ore));
     _TEST(_EQ(->resources[i].gas));
     _TEST(_EQ(->resources[i].used_psi));
@@ -521,11 +526,11 @@ bool detail::frameEq(replayer::Frame* f1, replayer::Frame* f2) {
     auto f1actions = elem.second;
     auto f2actions = f2->actions.at(elem.first);
     _TEST(_EQV(actions, .size()));
-    for (size_t i=0; i<f1actions.size(); i++) {
+    for (size_t i = 0; i < f1actions.size(); i++) {
       _TEST(_EQV(actions, [i].uid));
       _TEST(_EQV(actions, [i].aid));
       _TEST(_EQV(actions, [i].action.size()));
-      for (size_t k=0; k<f1actions[i].action.size(); k++)
+      for (size_t k = 0; k < f1actions[i].action.size(); k++)
         _TEST(_EQV(actions, [i].action[k]));
     }
   }
@@ -536,14 +541,14 @@ bool detail::frameEq(replayer::Frame* f1, replayer::Frame* f2) {
     std::sort(f1units.begin(), f1units.end(), detail::orderUnitByiD);
     std::sort(f2units.begin(), f2units.end(), detail::orderUnitByiD);
     _TEST(_EQV(units, .size()));
-    for (size_t i=0; i<f1units.size(); i++) {
+    for (size_t i = 0; i < f1units.size(); i++) {
 #define _GEN_VAR(NAME, NUM) _TEST(_EQV(units, [i].NAME));
       _DOALL(_GEN_VAR)
 #undef _GEN_VAR
       _TEST(_EQV(units, [i].velocityX));
       _TEST(_EQV(units, [i].velocityY));
       _TEST(_EQV(units, [i].orders.size()));
-      for (size_t k=0; k<f1units[i].orders.size(); k++)
+      for (size_t k = 0; k < f1units[i].orders.size(); k++)
         _TEST(_EQV(units, [i].orders[k]));
     }
   }

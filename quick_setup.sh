b3eab45df30b6acb90c145685fa64fc765a994aa
Nantas Nardelli <nantas.nardelli@gmail.com>
2017-03-18 08:36:10 -0700

    Bump minor version

--- quick_setup.sh
+++ quick_setup.sh
@@ -21,10 +21,10 @@ cp $TC_DIR/BWEnv/bin/*.dll ./
 cp $TC_DIR/BWEnv/bin/*.sh ./
 cp $TC_DIR/config/*.ini $SC_DIR/bwapi-data/
 cp $TC_DIR/maps/micro/ $SC_DIR/Maps/BroodWar/ -r
-curl -LO https://github.com/TorchCraft/TorchCraft/releases/download/v1.0-2/torchcraft-v1.0-2.zip
-unzip torchcraft-v1.0-2.zip
-cp torchcraft-v1.0-2/* ./
-cp torchcraft-v1.0-2/bin/* ./
+curl -LO https://github.com/TorchCraft/TorchCraft/releases/download/v1.2-1/torchcraft-v1.2-1.zip
+unzip torchcraft-v1.2-1.zip
+cp torchcraft-v1.2-1/* ./
+cp torchcraft-v1.2-1/bin/* ./
 
 if [[ `uname` == 'Darwin' ]]; then
     sed -i '' "s|^ai *=.*|ai=$SC_DIR/BWEnv.dll|" bwapi-data/bwapi.ini

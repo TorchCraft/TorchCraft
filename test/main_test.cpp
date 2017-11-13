00472d5a15dee6852153110cb2154fe9d20a3ca4
dgant <dgant@users.noreply.github.com>
2017-11-13 14:17:45 -0700

    Starting to fill out tests

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -10,29 +10,43 @@
 #include <iostream>
 #include "lest/lest.hpp"
 #include "frame.h"
+#include "flatbuffers.h"
 
-const lest::test specification[] = {
-  lest_CASE("A TorchCraft frame is invariant through serialization") {
-    SETUP("Create a TorchCraft frame") {
-      auto foo = 3;
+namespace torchcraft {
+namespace replayer {
 
-      EXPECT(foo == 3);
+  const lest::test specification[] = {
+    lest_CASE("A TorchCraft frame is invariant through serialization") {
+      SETUP("Create & serialize a frame") {
+        torchcraft::replayer::Frame f0;
+        torchcraft::replayer::Frame f1;
 
-      SECTION("Passing tests") {
-        EXPECT(0 == 0);
-        EXPECT(true);
+        flatbuffers::FlatBufferBuilder builder;
+        f0.addToFlatBufferBuilder(builder);
+
+
+
+        f0.reward += 1.23;
+        f0.is_terminal = ! f0.is_terminal;
+
+        std::stringstream buffer;
+        buffer << f0;
+        buffer >> f1;
+
+        EXPECT(f0.reward == f1.reward);
+        EXPECT(f0.is_terminal == f1.is_terminal);
+        EXPECT(f0.creep_map == f1.creep_map);
       }
-      SECTION("Failing tests") {
-        EXPECT(0 == 1);
-        EXPECT(false);
-      }
-    }
-  },
-};
+    },
+  };
+} // namespace torchcraft
+} // namespace replayer
 
 
-// Run with "ctest test --output-on-failure"
+// For output, run either as:
+// > ctest test --output-on-failure
+// > make ARGS='-VV' test
 int main(int argc, char* argv[]) {
   std::cout << "Running tests with console output" << std::endl;
-  return lest::run(specification, argc, argv, std::cerr);
+  return lest::run(torchcraft::replayer::specification, argc, argv, std::cerr);
 }

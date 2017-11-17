2ba8f63e34b74ee959066061aa5e2ebf34aac94b
dgant <dgant@users.noreply.github.com>
2017-11-17 11:02:00 -0700

    Updated flatbuffer compilation script to warn about flatc version mismatches

--- BWEnv/fbs/gen.sh
+++ BWEnv/fbs/gen.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
-flatc -c --gen-mutable --scoped-enums --gen-object-api --no-includes messages.fbs
-sed -i 's,\#include "flatbuffers/flatbuffers.h",\#include "flatbuffers.h",' messages_generated.h
+#!/bin/bash
+flatcVersionRequired='1.7.1';
+flatcVersionCurrent=`flatc --version`;
+if [[ $flatcVersionCurrent == *"$flatcVersionRequired"* ]]; then
+  flatc -c --gen-mutable --scoped-enums --gen-object-api --no-includes *.fbs
+  sed -i 's,\#include "flatbuffers/flatbuffers.h",\#include "flatbuffers.h",' *.h
+else
+  echo "The current flatbuffer headers require flatc version $flatcVersionRequired."
+  echo "Current flatbuffer version: $flatcVersionCurrent"
+  echo "You'll either want to get a compatible copy of flatc or update the headers."
+fi

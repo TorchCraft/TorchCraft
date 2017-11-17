dd2f82b15d1c38e926d7f173f512f72bbe90bb55
dgant <dgant@users.noreply.github.com>
2017-11-17 17:42:25 +0000

    Updated to latest version of FlatBuffers.

--- BWEnv/fbs/gen.sh
+++ BWEnv/fbs/gen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 flatc -c --gen-mutable --scoped-enums --gen-object-api --no-includes messages.fbs
-sed -i "" 's,\#include "flatbuffers/flatbuffers.h",\#include "flatbuffers.h",' messages_generated.h
+sed -i 's,\#include "flatbuffers/flatbuffers.h",\#include "flatbuffers.h",' messages_generated.h

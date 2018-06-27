6ceb5e4b56785adf5fe89846597b0305000862f3
Jonas Gehring <jgehring@fb.com>
2018-06-27 09:53:47 +0000

    Enable sharing of ZMQ context between active connections
    For training runs, we can easily have >80 active TorchCraft connections per
    process (e.g. >40 self-play games running in parallel). Constantly constructing
    and destructing a ZeroMQ context for each connection is both a waste of time and
    resources, since each context will result in a dedicated I/O thread. This change
    enables context sharing across across *active* connections -- once the last
    connection is closed, the shared context is destroyed and re-created on the
    next connection attempt.
    
    I did some basic benchmarking, and for a relatively simple self-play game I saw
    500 MBit going over the wire in total (send/receive for both players) within 30
    seconds. Assuming twice the size for a more complex game, we'd hit 1 Gbps with
    30 parallel games. The ZeroMQ FAQ (http://zeromq.org/area:faq#toc7) suggest to
    use one thread for every Gpbs so I went with two threads to be safe. In reality,
    we're often performing other operations while playing, which slows down the game
    and reduces the required throughput. If the game gets more complex, player steps
    will be slower as well.
    
    ZeroMQ contexts are not designed to be created and destroyed frequently anyway;
    they should be retained for a longer time. On the other hand, for a library, I
    still believe that it's good design to not allocate a lot of global resources
    which are supposed to be cleaned up at program exit. Hence, the solution here
    seems like a good compromise.
    
    This also includes setting the "linger" period for connection sockets. This is
    infinite by default, but now that the context can outlive the connection we
    don't want pending messages to linger in the context for too long.

--- client/connection.cpp
+++ client/connection.cpp
@@ -9,41 +9,45 @@
 
 #include <iostream>
 #include <sstream>
+#include <thread>
 
 #include "connection.h"
 
 namespace torchcraft {
 
 static const std::string ERRMSG_TIMEOUT_EXCEEDED = "Timeout exceeded";
+zmq::context_t* Connection::context_ = nullptr;
+int Connection::contextRef_ = 0;
+std::mutex Connection::contextMutex_;
 
-Connection::Connection(
-    const std::string& hostname,
-    int port,
-    int timeoutMs)
-    : sock_(ctx_, zmq::socket_type::req) {
+Connection::Connection(const std::string& hostname, int port, int timeoutMs)
+    : ctx_(refContext()), sock_(ctx_, zmq::socket_type::req) {
   std::ostringstream ss;
   ss << "tcp://" << hostname << ":" << port;
   sock_.setsockopt(ZMQ_SNDTIMEO, &timeoutMs, sizeof(timeoutMs));
   sock_.setsockopt(ZMQ_RCVTIMEO, &timeoutMs, sizeof(timeoutMs));
+  sock_.setsockopt(ZMQ_LINGER, 10);
   sock_.setsockopt(ZMQ_IPV6, 1);
   sock_.connect(ss.str());
 } // Connection
 
-Connection::Connection(
-    const std::string& file_socket,
-    int timeoutMs)
-    : sock_(ctx_, zmq::socket_type::req) {
+Connection::Connection(const std::string& file_socket, int timeoutMs)
+    : ctx_(refContext()), sock_(ctx_, zmq::socket_type::req) {
   std::ostringstream ss;
   ss << "ipc://" << file_socket;
   sock_.setsockopt(ZMQ_SNDTIMEO, &timeoutMs, sizeof(timeoutMs));
   sock_.setsockopt(ZMQ_RCVTIMEO, &timeoutMs, sizeof(timeoutMs));
+  sock_.setsockopt(ZMQ_LINGER, 10);
   sock_.setsockopt(ZMQ_IPV6, 1);
   sock_.connect(ss.str());
 } // Connection
 
 Connection::Connection(Connection&& conn)
-    : ctx_(std::move(conn.ctx_)), sock_(std::move(conn.sock_)) {}
+    : ctx_(refContext()), sock_(std::move(conn.sock_)) {}
 
+Connection::~Connection() {
+  derefContext();
+}
 
 bool Connection::send(const std::string& data) {
   clearError();
@@ -133,6 +137,39 @@ bool Connection::poll(long timeout) {
 void Connection::clearError() {
   errnum_ = 0;
   errmsg_.clear();
+}
+
+zmq::context_t& Connection::refContext() {
+  std::lock_guard<std::mutex> guard(contextMutex_);
+  contextRef_++;
+  if (contextRef_ == 1) {
+    if (context_ != nullptr) {
+      throw std::runtime_error("ZMQ context already initialized");
+    }
+    // Quote from http://zeromq.org/area:faq#toc7
+    // "The basic heuristic is to allocate 1 I/O thread in the context for every
+    // gigabit per second of data that will be sent and received (aggregated).
+    // Further, the number of I/O threads should not exceed (number_of_cpu_cores
+    // - 1)."
+    // We might hit 1 Gbit/s if we run a lot of non-trivial self-play games on a
+    // machine
+    // with many cores (say, 80 logical ones). So we'll ideally go for 2
+    // threads,
+    // subject to the number of cores, but fall back to 1 for small systems.
+    auto numIoThreads =
+        std::max(1, std::min(2, int(std::thread::hardware_concurrency()) - 1));
+    context_ = new zmq::context_t(numIoThreads);
+  }
+  return *context_;
+}
+
+void Connection::derefContext() {
+  std::lock_guard<std::mutex> guard(contextMutex_);
+  contextRef_--;
+  if (contextRef_ == 0) {
+    delete context_;
+    context_ = nullptr;
+  }
 }
 
 } // namespace torchcraft

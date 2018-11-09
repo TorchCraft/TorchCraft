a28b153ff10e1826f531d407f723c5e0ccbd488b
syhw <gabriel.synnaeve@gmail.com>
2018-11-09 14:20:09 -0500

    CODE_OF_CONDUCT.md and copyrights

--- BWEnv/bin/launch_x_sc.sh
+++ BWEnv/bin/launch_x_sc.sh
@@ -1,3 +1,10 @@
+# Copyright (c) 2015-present, Facebook, Inc.
+# All rights reserved.
+# 
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
 # This launches 5 instances of starcraft all operating on respective ports
 #    ./launch_x_sc.sh 5
 # Sleeping between different launches is purely useful for self_play!
--- docker/common/quick_start.sh
+++ docker/common/quick_start.sh
@@ -1,3 +1,10 @@
+# Copyright (c) 2015-present, Facebook, Inc.
+# All rights reserved.
+# 
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
 STARCRAFT_DIR=${WINEPREFIX:-~/.wine}/drive_c/StarCraft
 if [ ! -e $STARCRAFT_DIR ]; then
   echo "Starcraft dir not found... maybe you should install it to C:/ first!"
